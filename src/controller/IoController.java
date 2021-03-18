d0a5b12024cc55c6cff3d13b77e68dae062b4ec5
Rui Yan <rui.yan@tu-dortmund.de>
2021-03-18 11:05:34 +0100

    GameController ergaenzen

--- src/controller/IoController.java
+++ src/controller/IoController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import model.CardStack;
+
 /**
  * Verwaltet die IO.
  */
@@ -26,7 +28,7 @@ public class IoController {
 	 * @param path
 	 * 		Bekommt den Ort an dem sich das Kartendeck befindet übergeben.
 	 */
-	public void loadCardDeck(String path) {
+	public CardStack loadCardDeck(String path) {
 
 	}
 
