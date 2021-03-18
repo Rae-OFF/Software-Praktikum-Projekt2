a16006576cd24fa350c19b52995fc15531004678
Rui Yan <rui.yan@tu-dortmund.de>
2021-03-18 16:59:46 +0100

    CardControllerTest and GameControllerTest

---
+++ tests/controller/CardControllerTest.java
@@ -0,0 +1,80 @@
+package controller;
+
+import model.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static model.ActionType.SKIP;
+import static model.PlayerType.HUMAN;
+import static org.junit.Assert.*;
+
+import controller.MainController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void execAdmiral() {
+    }
+
+    @Test
+    public void execJester() {
+
+    }
+
+    @Test
+    public void execTrader() {
+    }
+
+    @Test
+    public void drawCard() {
+    }
+
+    @Test
+    public void acceptship() {
+    }
+
+    @Test
+    public void pop() {
+    }
+
+    @Test
+    public void taxIncrease() {
+    }
+
+    @Test
+    public void takeShip() {
+    }
+
+    @Test
+    public void buyPerson() {
+    }
+
+    @Test
+    public void defend() {
+    }
+
+    @Test
+    public void startExpedition() {
+    }
+
+    @Test
+    public void skip() {
+    }
+
+    @Test
+    public void shuffle() {
+    }
+}
---
+++ tests/controller/GameControllerTest.java
@@ -0,0 +1,70 @@
+package controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void currentMove() {
+    }
+
+    @Test
+    public void popCardPile() {
+    }
+
+    @Test
+    public void changeActor() {
+    }
+
+    @Test
+    public void finishRound() {
+    }
+
+    @Test
+    public void init() {
+    }
+
+    @Test
+    public void generateMove() {
+    }
+
+    @Test
+    public void getPossibleActions() {
+    }
+
+    @Test
+    public void undo() {
+    }
+
+    @Test
+    public void redo() {
+    }
+
+    @Test
+    public void shuffleDiscardPile() {
+    }
+
+    @Test
+    public void finishGame() {
+    }
+
+    @Test
+    public void isZonked() {
+    }
+
+    @Test
+    public void changeActivePlayer() {
+    }
+}
