23077600ae6e463b5f49b988613dc8f7618f123e
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2021-03-19 11:24:49 +0100

    HighscoreControllerTest hinzugefügt

---
+++ tests/controller/HighscoreControllerTest.java
@@ -0,0 +1,41 @@
+package controller;
+
+import model.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HighscoreControllerTest {
+
+    /**
+     * Testumgebung.
+     */
+    @Before
+    public void setUp(){
+
+    }
+
+    /**
+     * Konstruktor Test.
+     */
+    @Test
+    public void testHighscoreControllerKonstruktor(){
+
+    }
+
+    /**
+     * Testet das erfolgreiche holen der Highscoreliste.
+     */
+    @Test
+    public void getHighscoreListTest(){
+
+    }
+
+    /**
+     * Testet das erfolgreiches hinzufügen eines Spielers zu der Liste.
+     */
+    @Test
+    public void addPlayerScoreTest(){
+
+    }
+}
