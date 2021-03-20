2596e707b0d4ecdf4e0eace3d8a15bbeccb3e1af
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2021-03-20 17:03:40 +0100

    Model Card JavaDoc

--- src/model/Card.java
+++ src/model/Card.java
@@ -1,7 +1,21 @@
 package model;
 
+/**
+ * Interface für alle Karten.
+ */
 public interface Card {
+
+    /**
+     * Vergleichsmethode.
+     * @param card
+     *      Bekommt eine Karte übergeben.
+     * @return Gibt zurück ob die übergebene Karte die gleiche ist.
+     */
     boolean equals(Card card);
 
+    /**
+     *
+     * @return Gibt einen String zurück.
+     */
     String toString();
 }
