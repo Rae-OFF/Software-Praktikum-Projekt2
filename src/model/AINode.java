24caee3f61aa9394462f3fcb716d83b1ef45928e
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2021-03-22 22:57:48 +0100

    JavaDoc AINode

--- src/model/AINode.java
+++ src/model/AINode.java
@@ -5,25 +5,44 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Knoten einer KI.
+ */
 public class AINode {
     Move currentMove;
     AINode parent;
     Map<Action,AINode> branches;
 
+    /**
+     * Konstruktor.
+     * @param move Bekommt einen Zug übergeben.
+     */
     public AINode(Move move){
         this.currentMove = move;
         this.branches = new HashMap<Action, AINode>();
 
     }
 
+    /**
+     *
+     * @return Gibt zurück ob der Knoten ein Blatt ist (true) oder nicht (false).
+     */
     public boolean isLeaf(){
         return this.branches.isEmpty();
     }
 
+    /**
+     *
+     * @return Gibt den aktuellen Zug zurück.
+     */
     public Move getCurrentMove(){
         return this.currentMove;
     }
 
+    /**
+     *
+     * @return Gibt die Tiefe zurück.
+     */
     public int getDepth(){
 
         if(this.isLeaf()){
@@ -45,18 +64,36 @@ public class AINode {
         }
     }
 
+    /**
+     * Fügt dem Knoten ein Kind hinzu.
+     * @param action Bekommt eine Aktion übergeben.
+     * @param move Bekommt einen Zug übergeben.
+     */
     public void addChild(Action action, Move move){
         this.branches.put(action, new AINode(move));
     }
 
+    /**
+     *
+     * @return Gibt eine Liste aller Kinder des Knoten wieder.
+     */
     public List<AINode> getChildren(){
         return new ArrayList<>(branches.values());
     }
 
+    /**
+     *
+     * @return Gibt eine Liste aller Aktionen des Knoten wieder.
+     */
     public List<Action> getActions(){
         return new ArrayList<>(branches.keySet());
     }
 
+    /**
+     *
+     * @param action Bekommt eine Aktion übergeben.
+     * @return Gibt das entsprechende Kind des Knoten zurück.
+     */
     public AINode getChild(Action action){
         return this.branches.get(action);
     }
