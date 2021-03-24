c494657a393e95ccc7d8f1ac2965361135e09e88
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2021-03-24 20:23:35 +0100

    Test JavaDoc

--- tests/controller/IoControllerTest.java
+++ tests/controller/IoControllerTest.java
@@ -5,28 +5,51 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * Test für den IOController.
+ */
 public class IoControllerTest {
 
+    /**
+     * Erzeugt eine Testumgebung.
+     * @throws Exception
+     *      Kann eine Exception werfen.
+     */
     @Before
     public void setUp() throws Exception {
     }
 
+    /**
+     * Testet das Laden eines Spiels.
+     */
     @Test
     public void load() {
     }
 
+    /**
+     * Testet das Speichern eines Spiels.
+     */
     @Test
     public void save() {
     }
 
+    /**
+     * Testet das Protokollieren eines Spiels.
+     */
     @Test
     public void log() {
     }
 
+    /**
+     * Testet das Protokollieren eines Spiels.
+     */
     @Test
     public void testLog() {
     }
 
+    /**
+     * Testet das Laden eines Kartendecks.
+     */
     @Test
     public void loadCardDeck() {
     }
