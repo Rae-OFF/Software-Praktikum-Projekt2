fee2ff8669c052cabf6e7bf94e6559ed54868cf8
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2021-03-24 20:14:52 +0100

    Test JavaDoc

--- tests/controller/MediumAiTest.java
+++ tests/controller/MediumAiTest.java
@@ -5,12 +5,23 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * Test für die mittlere KI.
+ */
 public class MediumAiTest {
 
+    /**
+     * Erzeugt eine Testumgebung.
+     * @throws Exception
+     *      Worft eine Exception.
+     */
     @Before
     public void setUp() throws Exception {
     }
 
+    /**
+     * Testet das holen einer Aktion.
+     */
     @Test
     public void getAction() {
     }
