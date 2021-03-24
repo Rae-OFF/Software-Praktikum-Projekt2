3ecf8dd14be810d10237bc559348942bb08f234c
Rui Yan <rui.yan@tu-dortmund.de>
2021-03-24 10:12:32 +0100

    test class ergaezen.

---
+++ tests/controller/CardFactoryTest.java
@@ -0,0 +1,89 @@
+package controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CardFactoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void generateBlueShips() {
+    }
+
+    @Test
+    public void generateRedShips() {
+    }
+
+    @Test
+    public void generateBlackShips() {
+    }
+
+    @Test
+    public void generateYellowShips() {
+    }
+
+    @Test
+    public void generateGreenShips() {
+    }
+
+    @Test
+    public void generateAdmiral() {
+    }
+
+    @Test
+    public void generateTrader() {
+    }
+
+    @Test
+    public void generateSettlerCaptain() {
+    }
+
+    @Test
+    public void generatePriest() {
+    }
+
+    @Test
+    public void generateJackOfAllTrader() {
+    }
+
+    @Test
+    public void generatePirate() {
+    }
+
+    @Test
+    public void generateJester() {
+    }
+
+    @Test
+    public void generateSailor() {
+    }
+
+    @Test
+    public void generateMademoiselles() {
+    }
+
+    @Test
+    public void generateGovernor() {
+    }
+
+    @Test
+    public void generateTaxIncrease() {
+    }
+
+    @Test
+    public void generateExpedition() {
+    }
+
+    @Test
+    public void newCardsWithoutSpecial() {
+    }
+
+    @Test
+    public void newCardsWithSpecial() {
+    }
+}
---
+++ tests/controller/PlayerControllerTest.java
@@ -0,0 +1,37 @@
+package controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void executeAction() {
+    }
+
+    @Test
+    public void setPlayerOrder() {
+    }
+
+    @Test
+    public void getHint() {
+    }
+
+    @Test
+    public void getVictoryPoints() {
+    }
+
+    @Test
+    public void getCoins() {
+    }
+
+    @Test
+    public void countExpeditions() {
+    }
+}
